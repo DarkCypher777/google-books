c127c69faebd21929949c1d2435145c29d3cbac8
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-11-26 10:43:57 -0500

    heroku fix2

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,10 +12,12 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-app.use(routes)
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+mongoose.Promise = Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/google-books-db',
+  { useNewUrlParser: true }
+);
 
-app.listen(PORT, () => {
-    console.log("🌎 ==> API server now on port ${PORT}!");   
-});
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
